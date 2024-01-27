48934f69308a8ea45c7f524acc6d916ad678c781
FahadAwanKamra <91179741+FahadAwanKamra@users.noreply.github.com>
2024-01-27 16:05:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923276096349', 'FAHAD', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
